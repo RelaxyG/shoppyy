195ca119d9ee9621684d56eccd98a5523900d7f3
relaxyg <relaxyg4@gmail.com>
2022-04-13 23:11:21 +0600

    config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }

--- db/migrate/20220413170148_devise_create_users.rb
+++ db/migrate/20220413170148_devise_create_users.rb
@@ -35,7 +35,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       t.string :first_name
       t.string :last_name
       t.string :address
-      t.string :email
 
       t.timestamps null: false
     end
