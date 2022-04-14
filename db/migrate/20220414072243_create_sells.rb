e9881cda20eca8a79169f4d8bf46c65f618f483d
relaxyg <relaxyg4@gmail.com>
2022-04-14 13:23:12 +0600

    g sell & buy models

--- db/migrate/20220414072243_create_sells.rb
+++ db/migrate/20220414072243_create_sells.rb
@@ -2,6 +2,9 @@ class CreateSells < ActiveRecord::Migration[6.1]
   def change
     create_table :sells do |t|
       t.references :item, null: false, foreign_key: true
+      t.string :name
+      t.text :description
+      t.integer :amount
 
       t.timestamps
     end
--- db/migrate/20220414072255_create_buys.rb
+++ db/migrate/20220414072255_create_buys.rb
@@ -2,6 +2,9 @@ class CreateBuys < ActiveRecord::Migration[6.1]
   def change
     create_table :buys do |t|
       t.references :item, null: false, foreign_key: true
+      t.string :name
+      t.text :description
+      t.integer :amount
 
       t.timestamps
     end
